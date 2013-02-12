59a8cd8d8288340c47a17f3245a41539d1e14f5a
EvilTosha <toshaevil@gmail.com>
2013-02-12 15:43:07 +0400

    truly blank cursor

--- Main.cpp
+++ Main.cpp
@@ -2,13 +2,17 @@
 #include <QStyleFactory>
 #include <QDebug>
 #include <QStringList>
+#include <QWSServer>
 
 int main(int argc, char *argv[])
 {
 	QApplication a(argc, argv);
 
+#ifdef Q_WS_QWS
+	QWSServer::setCursorVisible( false );
+#endif
+
 	a.setStyle(QStyleFactory::create("Cleanlooks"));
-	a.setOverrideCursor(Qt::BlankCursor);
 
 	MainWindow w;
 	w.show();
