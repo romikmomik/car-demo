be215d59ec2807b93d08891dce32f354f61149cb
Anton Samoylov <toshaevil@gmail.com>
2013-02-26 02:58:50 +0400

    write int to motors control (not double)

--- CopterMotor.cpp
+++ CopterMotor.cpp
@@ -15,7 +15,7 @@ void CopterMotor::invoke(int _power)
 	QString s;
 	m_ctrlFile.open(QIODevice::WriteOnly|QIODevice::Truncate|QIODevice::Unbuffered|QIODevice::Text);
 	double powerFactor = (double)(m_powerMax - m_powerMin) / 100;
-	s.sprintf("%lf\n", _power * powerFactor + m_powerMin);
+	s.sprintf("%d\n", static_cast<int>(_power * powerFactor + m_powerMin));
 	m_ctrlFile.write(s.toLatin1());
 	m_ctrlFile.close();
 }
